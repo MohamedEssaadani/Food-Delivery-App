5e60d2c4a290104a3124758bf244b633821d0bc4
Es-saadani Mohamed <40544605+MohamedEssaadani@users.noreply.github.com>
2021-02-18 12:37:09 +0100

    Restaurant Details

--- frontend/src/views/RestaurantDetailsView.js
+++ frontend/src/views/RestaurantDetailsView.js
@@ -1,4 +1,6 @@
 import React, { useEffect, useState } from "react"
+import { Link } from "react-router-dom"
+import { Row, Col, Image, ListGroup, Card, Button } from "react-bootstrap"
 import axios from "axios"
 
 function RestaurantDetails({ match }) {
@@ -16,7 +18,47 @@ function RestaurantDetails({ match }) {
 
   return (
     <>
-      <h3>{restaurant.name} Details </h3>
+      <Link className="btn btn-dark my-3" to="/">
+        Back
+      </Link>
+      <Row>
+        <Col md={6}>
+          <Image src={restaurant.picture} alt={restaurant.name} fluid />
+        </Col>
+        <Col md={3}>
+          <ListGroup variant="flush">
+            <ListGroup.Item>
+              <h3>{restaurant.name}</h3>
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <strong>Rating: </strong> {restaurant.rating}
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <strong>description: </strong> {restaurant.description}
+            </ListGroup.Item>
+          </ListGroup>
+        </Col>
+        <Col md={3}>
+          <Card>
+            <ListGroup variant="flush">
+              <ListGroup.Item>
+                <strong>Ville: </strong> {restaurant.ville}
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <strong>Address: </strong> {restaurant.address}
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <strong>Phone: </strong> {restaurant.phone}
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Button type="button" className="btn-block">
+                  See Meenu
+                </Button>
+              </ListGroup.Item>
+            </ListGroup>
+          </Card>
+        </Col>
+      </Row>
     </>
   )
 }

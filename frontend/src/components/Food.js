8d11110692752f915ab4a79ce9b51ca09719723a
Es-saadani Mohamed <40544605+MohamedEssaadani@users.noreply.github.com>
2021-03-28 18:52:40 +0100

    cart

--- frontend/src/components/Food.js
+++ frontend/src/components/Food.js
@@ -1,25 +1,39 @@
 import React from "react"
-import { Card } from "react-bootstrap"
+import { Button, Card } from "react-bootstrap"
+import { Link } from "react-router-dom"
+
 import Rating from "./Rating.js"
 
 function Food({ food }) {
+  const addToCart = () => {
+    console.log("Add")
+  }
   return (
     <>
       <Card className="my-3 p-3 rounded">
-        <Card.Img
-          src={`/images/${food.image}`}
-          alt={food.name}
-          height="140px"
-          width="140px"
-          variant="top"
-        ></Card.Img>
+        <Link to="/">
+          <Card.Img
+            src={`/images/${food.image}`}
+            alt={food.name}
+            height="140px"
+            width="140px"
+            variant="top"
+          ></Card.Img>
+        </Link>
         <Card.Body>
-          <Card.Title as="div">
-            <strong>{food.name}</strong>
-          </Card.Title>
+          <Link to="/">
+            <Card.Title as="div">
+              <strong>{food.name}</strong>
+            </Card.Title>
+          </Link>
           <Rating value={food.rating} />
           <Card.Text>
             <span> Prix: ${food.price}</span>
+            <i
+              className="fas fa-shopping-cart"
+              style={{ float: "right", cursor: "pointer" }}
+              onClick={addToCart}
+            ></i>
           </Card.Text>
         </Card.Body>
       </Card>
